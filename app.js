633821b469fb6e470783d1e4352ce9f9137c865f
Oskar Woof <oskarwoof7@gmail.com>
2021-10-11 09:12:44 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let navToggle = document.querySelector(".nav-toggle");
+let links = document.querySelector(".links");
+let secLinks = document.querySelectorAll("section");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+links.addEventListener("click", function () {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  }
+});
+
+console.log(secLinks);
+
