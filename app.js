ddc32cc035acf943f8fabe2207d25fce0d5610f7
Oskar Woof <oskarwoof7@gmail.com>
2021-10-12 08:09:57 +0200

    some changes

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-let navToggle = document.querySelector(".nav-toggle");
-let links = document.querySelector(".links");
-let secLinks = document.querySelectorAll("section");
+// let navToggle = document.querySelector(".nav-toggle");
+// let links = document.querySelector(".links");
+// let secLinks = document.querySelectorAll("section");
 
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
+// navToggle.addEventListener("click", function () {
+//   links.classList.toggle("show-links");
+// });
+
+// links.addEventListener("click", function () {
+//   if (links.classList.contains("show-links")) {
+//     links.classList.remove("show-links");
+//   }
+// });
+
+// console.log(secLinks);
+
+$(document).on("click", 'a[href^="#"]', function (e) {
+  e.preventDefault();
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 72,
+      },
+      800,
+      "linear"
+    );
 });
-
-links.addEventListener("click", function () {
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  }
-});
-
-console.log(secLinks);
-
