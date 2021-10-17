164059e5403c0d7bff085b8874439d2642d5f91b
Oskar Woof <oskarwoof7@gmail.com>
2021-10-17 02:31:17 +0200

    some changes

--- app.js
+++ app.js
@@ -2,15 +2,43 @@
 let navToggle = document.querySelector(".nav-toggle");
 let links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
+//** */ Sakriva header kad scrolujes **//
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("header").style.top = "0";
+//   } else {
+//     document.getElementById("header").style.top = "-79px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
+// navToggle.addEventListener("click", function () {
+//   links.classList.toggle("show-links");
+// });
+
+// links.addEventListener("click", function () {
+//   if (links.classList.contains("show-links")) {
+//     links.classList.remove("show-links");
+//   }
+// });
+// $(".nav-toggle").click(function () {
+//   // console.log("pritaji se");
+//   $(".links").toggleClass("show-links");
+// });
+
+
+$(document).on("click", 'a[href^="#"]', function (e) {
+  e.preventDefault();
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 72
+      },
+      800,
+      "linear"
+    );
 });
-
-links.addEventListener("click", function () {
-  if (links.classList.contains("show-links")) {
-    links.classList.remove("show-links");
-  }
-});
-
-
-
